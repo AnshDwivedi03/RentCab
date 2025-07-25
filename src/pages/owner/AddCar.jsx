3299ed3814fee3268f1d092a58ae2549f7208576
Ansh Dwivedi <anshdhardwivediad@gmail.com>
2025-07-25 19:15:54 +0530

    Admin Dashboard updated

--- src/pages/owner/AddCar.jsx
+++ src/pages/owner/AddCar.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import 
 const AddCar = () => {
+  const [image,setImage]=useState(null);
+  const [car,setCar]=useState({
+    brand:'',
+    model:'',
+    year:0,
+    pricePerDay:0,
+    category:'',
+    transimssion:'',
+    fuel_type:'',
+    seating_capacity:0,
+    location:'',
+    decription:'',
+  })
   return (
     <div>
-      
+     
     </div>
   )
 }
